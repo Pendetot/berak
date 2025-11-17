f11bf50a2797703e6d08050ff047c285c983393d
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-11-17 22:24:30 +0700

    Allow dd argument

--- trans.sh
+++ trans.sh
@@ -6013,23 +6013,31 @@ EOF
     hypervisor_vendor=$(lscpu | grep 'Hypervisor vendor:' | awk '{print $3}')
     apk del lscpu
 
-    aws_pv_ver=$(
-        case "$nt_ver" in
-            6.1)
-                if $support_sha256; then echo 8.3.5; else echo 8.3.2; fi
-                ;;
-            6.2|6.3)
-                case "$hypervisor_vendor" in
-                    Microsoft) echo 8.4.3 ;;
-                    Xen) echo 8.3.5 ;;
-                    *) echo 8.3.5 ;;
-                esac
-                ;;
-            *)
-                echo Latest
-                ;;
-        esac
-    )
+    case "$nt_ver" in
+        6.1)
+            if $support_sha256; then
+                aws_pv_ver=8.3.5
+            else
+                aws_pv_ver=8.3.2
+            fi
+            ;;
+        6.2|6.3)
+            case "$hypervisor_vendor" in
+                Microsoft)
+                    aws_pv_ver=8.4.3
+                    ;;
+                Xen)
+                    aws_pv_ver=8.3.5
+                    ;;
+                *)
+                    aws_pv_ver=8.3.5
+                    ;;
+            esac
+            ;;
+        *)
+            aws_pv_ver=Latest
+            ;;
+    esac
 
     url=$(
         case "$aws_pv_ver" in
@@ -7058,6 +7066,9 @@ elif [ "$1" = "alpine" ]; then
     distro=alpine
     # 后面的步骤很多都会用到这个，例如分区布局
     cloud_image=0
+elif [ "$1" = "dd" ]; then
+    info 'switch to dd'
+    distro=dd
 elif [ -n "$1" ]; then
     error_and_exit "unknown option $1"
 fi
